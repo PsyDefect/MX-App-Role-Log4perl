3aa943902d39d55f68ee94b46f43e8252ed627d6
John Dexter <John.Dexter@mercer.com>
2014-05-23 12:03:22 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+utf-8# NAME
+
+MooseX::App::Role::Log4perl - It's new $module
+
+# SYNOPSIS
+
+    use MooseX::App::Role::Log4perl;
+
+# DESCRIPTION
+
+MooseX::App::Role::Log4perl is ...
+
+# LICENSE
+
+Copyright (C) John Dexter.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+John Dexter <John.Dexter@mercer.com>
---
+++ lib/MooseX/App/Role/Log4perl.pm
@@ -0,0 +1,39 @@
+package MooseX::App::Role::Log4perl;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MooseX::App::Role::Log4perl - It's new $module
+
+=head1 SYNOPSIS
+
+    use MooseX::App::Role::Log4perl;
+
+=head1 DESCRIPTION
+
+MooseX::App::Role::Log4perl is ...
+
+=head1 LICENSE
+
+Copyright (C) John Dexter.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+John Dexter E<lt>John.Dexter@mercer.comE<gt>
+
+=cut
+
